9b18cbd20c5728d21b48458b9bc3cdcb77efe5c7
Fatih Çetiner <f-cetiner99@hotmail.com>
2024-02-06 01:22:11 +0300

    Welcome

--- app/src/main/java/com/example/ftechnology/FTechnologyApp.kt
+++ app/src/main/java/com/example/ftechnology/FTechnologyApp.kt
@@ -5,3 +5,32 @@ import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
 class FTechnologyApp : Application()
+
+/**
+ * ┌─────────────────────────────────────────────────────────────────────┐
+ * │                                                                     │
+ * │                 🚀 FTechnologyApp - Application Structure 🚀        │
+ * │                                                                      │
+ * │   This class incorporates powerful technologies such as Dagger Hilt, │
+ * │   Navigation, Room, Retrofit, MVVM, Clean Architecture, Lottie, and  │
+ * │   OkHttp, following modern Android development practices.            │
+ * │                                                                      │
+ * │   🛠️ Dagger Hilt: Facilitates dependency injection.                  │
+ * │   🚀 Navigation: Manages screen navigation.                          │
+ * │   🗄️ Room: Provides database access.                                 │
+ * │   🌐 Retrofit: Communicates with APIs.                               │
+ * │   🏗️ MVVM: Modular and testable architecture.                        │
+ * │   🏛️ Clean Architecture: Adheres to layered architectural principles.│
+ * │   🎨 Lottie: Integrates animations, enhances visual appeal.          │
+ * │   🌐 OkHttp: Manages network requests.                               │
+ * │   🚀 Firebase: Utilizes Firebase for cloud services.                 │
+ * │                                                                      │
+ * │                "Code quality and performance are prioritized."       │
+ * │                                                                     │
+ * └─────────────────────────────────────────────────────────────────────┘
+ *
+ *                          🚀 Developer Information 🚀
+ *   - Medium:   [ @FatihCetiner ](https://medium.com/@FatihCetiner)
+ *   - GitHub:   [ fatihCetiner ](https://github.com/fatihCetiner)
+ *   - LinkedIn: [ Fatih Furkan Çetiner ](https://www.linkedin.com/in/fatih-furkan-%C3%A7etiner-174b3218a/)
+ */
